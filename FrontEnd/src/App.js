fe314568490825524a2628cdf32cb15d0c10d3bf
Ruban230 <103724420+Ruban230@users.noreply.github.com>
2025-02-22 02:24:56 +0500

    navbar,about,Services(FrontEnd)

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Services from './Components/Services';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <About/>
+      <Services/>
+      
     <Home/>
     </div>
   );
